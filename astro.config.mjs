b29fa380de0722a9cc28ce3d801a0c1a7cbcc001
Nigel Stirzaker <nigel.stirzaker@nielseniq.com>
2025-11-10 13:05:51 +0000

    post merge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
+
+import node from '@astrojs/node';
 import tailwindcss from "@tailwindcss/vite";
-
-
 
 // https://astro.build/config
 export default defineConfig({
