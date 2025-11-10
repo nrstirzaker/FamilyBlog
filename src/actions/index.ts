97f1552990271ac07b2a412d8d0b4666f932bc98
Nigel Stirzaker <nigel.stirzaker@nielseniq.com>
2025-11-10 19:56:43 +0000

    Can't connect to supabase/postgres back end on Vercel

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,7 +3,7 @@ import { z } from 'astro:schema';
 import { createClient } from '@supabase/supabase-js'
 
 
-const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY)
+const supabase = createClient(import.meta.env.SUPABASE_URL, import.meta.env.SUPABASE_KEY)
 
 export const server = {
     register: defineAction({
@@ -11,22 +11,31 @@ export const server = {
         input: z.object({
             familyName: z.string(),
             emailAddress: z.string().email(),
-            privacyStatement: z.boolean()
+            privacyStatement: z.coerce.boolean()
         }),
 
         handler: async (formData) => {
+            const yesPrivacy: boolean = true;
             console.log("inside handler")
-            console.log("formData:" + formData)
+            console.log("formData:email:" + formData.emailAddress)
+            console.log("formData:familyname:" + formData.familyName)
+            console.log("formData:privacy:" + formData.privacyStatement)
+            console.log(import.meta.env.SUPABASE_URL)
+            console.log(import.meta.env.SUPABASE_KEY)
+            // const { data, error } = await supabase
+            //     .from('AddressBook')
+            //     .insert([
+            //         {
+            //             email_address: formData.emailAddress,
+            //             family_name:formData.familyName
+            //
+            //         },
+            //     ])
+
             const { data, error } = await supabase
-                .from('AddressBook')
-                .insert([
-                    {
-                        email_address: formData.emailAddress,
-                        family_name:formData.familyName,
-                        privacy_statement: formData.privacyStatement
-                    },
-                ])
+                .from("public.address_book")
                 .select()
+
 
             if (error) {
                 console.error(error)
