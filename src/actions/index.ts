157d4b8247d00c9a7316b7897a86ab74f7055a6e
nrstirzaker <nrstirzaker.techblog@gmail.com>
2025-11-14 09:09:10 +0000

    added stirzaker sub domain and no working thankyou page

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,6 +2,7 @@ export const prerender = false
 import { defineAction } from 'astro:actions';
 import { z } from 'astro:schema';
 import { Pool } from 'pg';
+
 const pool = new Pool({
     connectionString: import.meta.env.DATABASE_URL,
     ssl: true,
@@ -27,18 +28,13 @@ export const server = {
             const client = await pool.connect();
             try {
                 const res = await client.query(insertSQL, values);
-                console.log(res.rows[0])
+
                 // TODO remove the above but add logging
                 // TODO redirect to thankyou page
                 // TODO send email confirming registration
             } finally {
                 client.release();
             }
+            console.log("navigate")
 
-
-
-
-
-        },
-    })
-}
+            return
--- src/lib/posts.js
+++ src/lib/posts.js
@@ -1,20 +1,20 @@
-import {supabase} from "./supabase.ts";
+import {supabase} from "../../lib/supabase.ts";
 
 
 export const getPosts = async function () {
     console.log("Getting posts");
-    let {data, error} = await supabase.from("Family-BlogPosts").select();
+    let {data, error} = await supabase.from("Family-BlogPosts").select("*");
 
-    // if (error) {
-    //     const message = {"error": error.message};
-    //     console.log(message);
-    // } else {
-        error = {msg: "a"}
-    //}
+    if (error) {
+        const message = {"error": error.message};
+        console.log(message);
+    } else {
+        error = {message: " "}
+    }
 
     if (!data) {
         data = []
     }
 
-    return {'data': data, "error": error}
+    return {'data': data, "error": error};
 }
